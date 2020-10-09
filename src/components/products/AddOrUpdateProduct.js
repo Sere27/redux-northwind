57603b4fd05183d0a982d9ee35e1043a8ca26100
Sere27 <vetbat1126@gmail.com>
2020-10-09 19:32:46 +0300

    mapstate, mapdispatch functions added

--- src/components/products/AddOrUpdateProduct.js
+++ src/components/products/AddOrUpdateProduct.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
+import { getCategories } from "../../redux/actions/categoryActions";
+import { saveProduct } from "../../redux/actions/productActions";
 
 function AddOrUpdateProduct({
 	products,
@@ -40,5 +42,40 @@ function AddOrUpdateProduct({
 			//history de daha önce geldiğimiz sayfalara yönlendirme yapmak için yöntem
 			history.push("/");
 		});
-	}
+   }
+   
+   return (
+      //******  burada artık ürün geliştirmeye yöneli textbox vs burada tasarlayacagım
+   )
 }
+
+export function getProductById(products, productId) {
+	let product = products.find((product) => product.id === productId) || null;
+	return product;
+}
+
+//react hooksu redux bağlamamız lazım birde
+
+//ownprops bizim componentlerin kendi içerilerinde barındırdıkları proplara karşılık geliyor
+// git parametlere bak ordan productid çek
+function mapStateToProps(state, ownProps) {
+	const productId = ownProps.match.params.productId;
+	//state ki ürünler içerisinde bu ürünü bulmaya calısıyoruz
+	const product =
+		productId && state.productReducer.length > 0
+			? getProductById(state.productReducer, productId)
+			: {};
+	return {
+		product,
+		products: state.productReducer,
+		categories: state.categoryReducer,
+	};
+}
+
+const mapDispatchToProps = {
+	//redux gidip reducer bakıp oradaki operasyonları çekme
+	getCategories,
+	saveProduct,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(AddOrUpdateProduct);
