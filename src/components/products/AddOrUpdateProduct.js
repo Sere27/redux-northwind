a23a2fb0bcbd2551341e33de72111afa1c730fb4
Sere27 <vetbat1126@gmail.com>
2020-10-09 19:02:43 +0300

    text input added

---
+++ src/components/products/AddOrUpdateProduct.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from "react";
+import { connect } from "react-redux";
+
+function AddOrUpdateProduct({
+	products,
+	categories,
+	getCategories,
+	getProducts,
+	saveProduct,
+	...props
+}) {}
